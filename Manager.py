79ebee68ee8b4d945c19c40b4aaedeca51eefb6b
moeiniamir <amirmoini@outlook.com>
2021-07-17 09:24:20 +0430

    fix parent calculation in Manager.py

--- Manager.py
+++ Manager.py
@@ -8,16 +8,15 @@ import re
 
 
 def handle(client_sock):
-    while True:
-        msg = loads(recv_message(client_sock))
-        m = re.match(r"(\d+) REQUESTS FOR CONNECTING TO NETWORK ON PORT (\d+)", msg)
-        id, port = int(m.group(1)), int(m.group(2))
-        parent_indx = len(nodes_list) // 2
-        nodes_list.append((id, port))
-        parent_id, parent_port = nodes_list[parent_indx] if parent_indx > 0 else -1, -1
-        msg = f'CONNECT TO {parent_id} WITH PORT {parent_port}'
-        send_message(client_sock, dumps(msg))
-        client_sock.close()
+    msg = loads(recv_message(client_sock))
+    m = re.match(r"(\d+) REQUESTS FOR CONNECTING TO NETWORK ON PORT (\d+)", msg)
+    id, port = int(m.group(1)), int(m.group(2))
+    parent_indx = (len(nodes_list) - 1) // 2
+    nodes_list.append((id, port))
+    parent_id, parent_port = nodes_list[parent_indx] if parent_indx > 0 else -1, -1
+    msg = f'CONNECT TO {parent_id} WITH PORT {parent_port}'
+    send_message(client_sock, dumps(msg))
+    client_sock.close()
 
 
 nodes_list = []
